100c54e92d73ff1516f0ea2b3581cddc5881c818
Sabino Rendón Mireles <sabino.rendon.mireles@outlook.es>
2020-04-21 16:07:24 -0500

    BUG NO CARGA DATA RESUELTO 2

--- Master/Rutas/cuentas/cuentas.js
+++ Master/Rutas/cuentas/cuentas.js
@@ -7,15 +7,15 @@ const validateCuenta = require('./../../Middleware/validate/validateCuenta');
 const userProfile = require('./../../Middleware/getUserProfile');
 
 const router = express.Router();
-router.get('/cuentas/clientes/listarA',[auth0, userProfile],  CuentasController.getCuentasCA);
-router.get('/cuentas/proveedores/listarA/:last?',[auth0, userProfile],  CuentasController.getCuentasPA);
-router.get('/cuentas/propietarios/listarA/:last?',[auth0, userProfile],  CuentasController.getPropietariosA);
-router.get('/cuentas/propietarios/buscar/:search',[auth0, userProfile],  CuentasController.getPropietario);
-router.get('/cuentas/propietarios/listar/:search',[auth0, userProfile],  CuentasController.getCuentasPropietarios);
-router.get('/cuentas/listarCuentas/:last?',[auth0, userProfile],  CuentasController.getCuentas);
-router.post('/cuentas/guardar', [auth0, userProfile ,validateCuenta], CuentasController.save);
-router.put('/cuenta/desactivar/:id',[auth0, userProfile],  CuentasController.hide);
-router.put('/cuenta/modificar/:id',[auth0, userProfile],  CuentasController.update);
-router.delete('/cuenta/elimiar/:id',[auth0, userProfile],  CuentasController.delete);
+router.get('/cuentas/clientes/listarA', CuentasController.getCuentasCA);
+router.get('/cuentas/proveedores/listarA/:last?', CuentasController.getCuentasPA);
+router.get('/cuentas/propietarios/listarA/:last?', CuentasController.getPropietariosA);
+router.get('/cuentas/propietarios/buscar/:search', [auth0, userProfile], CuentasController.getPropietario);
+router.get('/cuentas/propietarios/listar/:search', [auth0, userProfile], CuentasController.getCuentasPropietarios);
+router.get('/cuentas/listarCuentas/:last?', [auth0, userProfile], CuentasController.getCuentas);
+router.post('/cuentas/guardar', [auth0, userProfile, validateCuenta], CuentasController.save);
+router.put('/cuenta/desactivar/:id', [auth0, userProfile], CuentasController.hide);
+router.put('/cuenta/modificar/:id', [auth0, userProfile], CuentasController.update);
+router.delete('/cuenta/elimiar/:id', [auth0, userProfile], CuentasController.delete);
 
 module.exports = router;
