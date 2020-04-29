be5e937aa25d2f7c2f7e2b6b36f134930f2c653f
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-04-29 11:06:21 -0500

    test variables de entorno

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 var mongoose = require("mongoose");
 var app = require("./app");
 var port = process.env.PORT || 3002;
-const url_certificado = process.env.URL_CERT_PRODUCCION || "certs/desarrollo/llavePrivada.pem";
-const passphrase_certificado = process.env.PASSPHRASE_CERT_PRODUCCION || "mWEYKJ4Zdi";
+var url_certificado = process.env.URL_CERT_PRODUCCION || "certs/desarrollo/llavePrivada.pem";
+var passphrase_certificado = process.env.PASSPHRASE_CERT_PRODUCCION || "mWEYKJ4Zdi";
 
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
 app.listen(port, () => {
     console.log('Servidor corriendo en http://localhost:' + port);
-    console.log(url_certificado);
-    console.log(passphrase_certificado);
+    console.log('URL CERTIFICADO:' + url_certificado);
+    console.log('PASSPHRASE:' + passphrase_certificado);
 });
