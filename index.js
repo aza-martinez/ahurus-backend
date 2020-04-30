30c574b0ae5792082538bf63ba1e5c3578a4f5cb
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-04-30 10:01:53 -0500

    test procesos

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
 var mongoose = require("mongoose");
 var app = require("./app");
 var envJSON = require('./env.variables.json');
+console.log(envJSON);
 var node_env = process.env.NODE_ENV || 'development';
-var puerto = envJSON[node_env].PORT;
-var certificado = envJSON[node_env].CERTS_URL;
-var passphrase = envJSON[node_env].PASSPHRASE_CERT;
+console.log(process.env.NODE_ENV);
+console.log(node_env);
+
+if (node_env == "production") {
+    var puerto = envJSON[node_env].PORT_P;
+    var certificado = envJSON[node_env].CERTS_URL_P;
+    var passphrase = envJSON[node_env].PASSPHRASE_CERT_P;
+} else {
+    var puerto = envJSON[node_env].PORT_D;
+    var certificado = envJSON[node_env].CERTS_URL_D;
+    var passphrase = envJSON[node_env].PASSPHRASE_CERT_D;
+}
 
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
