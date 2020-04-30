38c52bdbd0517b186c4a8b916601621437fa4721
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-04-30 10:05:59 -0500

    ENTORNO DESARROLLO LISTO

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 var mongoose = require("mongoose");
 var app = require("./app");
 var envJSON = require('./env.variables.json');
-console.log(envJSON);
 var node_env = process.env.NODE_ENV || 'development';
-console.log(process.env.NODE_ENV);
-console.log(node_env);
+
 
 if (node_env == "production") {
     var puerto = envJSON[node_env].PORT_P;
@@ -22,6 +20,5 @@ mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
 app.listen(puerto, () => {
     console.log('Servidor corriendo en http://localhost: ' + puerto);
-    console.log('URL CERTIFICADO: ' + certificado);
-    console.log('PASSPHRASE: ' + passphrase);
+    console.log('ENTORNO: ' + node_env);
 });
