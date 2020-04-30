85a5e84939a6cdf5983c6d05ac441046cab3f57e
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-04-30 11:14:38 -0500

    AJUSTE FECHAS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var app = require("./app");
 var envJSON = require('./env.variables.json');
 var node_env = process.env.NODE_ENV || 'development';
-var puerto = process.env.PORT_P || 8080;
+var puerto = process.env.PORT || 8080;
 
 
 mongoose.set("useFindAndModify", false);
