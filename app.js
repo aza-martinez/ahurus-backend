9dc92a94e270f99c75b609257643ad338e4e98f7
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-05-17 12:56:52 -0500

    TEST

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
 'use strict';
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 var fs = require('fs');
 var app = express();
-var mongoose = require('mongoose');
-var https = require('https');
-var envJSON = require('./env.variables.json');
-var node_env = process.env.NODE_ENV || 'development';
-
-mongoose.set('useFindAndModify', false);
-mongoose.Promise = global.Promise;
-
-if (node_env == 'production') {
-  var puerto = envJSON[node_env].PORT_P;
-  var key = envJSON[node_env].KEY_SSL_P;
-  var cert = envJSON[node_env].CERT_SSL_P;
-  https
-    .createServer(
-      {
-        key: fs.readFileSync(key),
-        cert: fs.readFileSync(cert),
-      },
-      app
-    )
-    .listen(puerto, function() {
-      console.log('Servidor Ahurus Corriendo En: ' + puerto);
-      console.log('ENTORNO: ' + node_env);
-    });
-} else {
-  var puerto = envJSON[node_env].PORT_D;
-  var key = envJSON[node_env].KEY_SSL_D;
-  var cert = envJSON[node_env].CERT_SSL_D;
-  app.listen(puerto, function() {
-    console.log('Servidor Ahurus Corriendo En: ' + puerto);
-    console.log('ENTORNO: ' + node_env);
-  });
-}
 
 const usuario_routes = require('./Master/Rutas/usuarios/usuarios');
 const resources_routes = require('./Master/Rutas/resources/resources');
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var envJSON = require('./env.variables.json');
+var https = require('https');
+var node_env = process.env.NODE_ENV || 'development';
+//NODE_ENV=production npm start
+mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
+
+if (node_env == 'production') {
+  var puerto = envJSON[node_env].PORT_P;
+  var key = envJSON[node_env].KEY_SSL_P;
+  var cert = envJSON[node_env].CERT_SSL_P;
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(key),
+        cert: fs.readFileSync(cert),
+      },
+      app
+    )
+    .listen(puerto, function() {
+      console.log('Servidor Ahurus Corriendo En: ' + puerto);
+      console.log('ENTORNO: ' + node_env);
+    });
+} else {
+  var puerto = envJSON[node_env].PORT_D;
+  var key = envJSON[node_env].KEY_SSL_D;
+  var cert = envJSON[node_env].CERT_SSL_D;
+  app.listen(puerto, () => {
+    console.log('Servidor corriendo en http://localhost: ' + puerto);
+    console.log('ENTORNO: ' + node_env);
+  });
+}
