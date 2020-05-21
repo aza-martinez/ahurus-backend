883db1325b416d3707760730547b56be6c295d9e
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-05-21 10:23:33 -0500

    Bug run server

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var envJSON = require('./env.variables.json');
 var fs = require('fs');
 var https = require('https');
 var node_env = process.env.NODE_ENV || 'development';
+
+const puertoDev = envJSON[node_env].PORT_D;
+const keyDev = envJSON[node_env].KEY_SSL_D;
+const certDev = envJSON[node_env].CERT_SSL_D;
+const puerto = envJSON[node_env].PORT_P;
+const key = envJSON[node_env].KEY_SSL_P;
+const cert = envJSON[node_env].CERT_SSL_P;
 //NODE_ENV=production npm start
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -13,9 +20,6 @@ mongoose.Promise = global.Promise;
 //HTTPS
 
 if (node_env === 'production') {
-	const puerto = envJSON[node_env].PORT_P;
-	const key = envJSON[node_env].KEY_SSL_P;
-	const cert = envJSON[node_env].CERT_SSL_P;
 	https
 		.createServer(
 			{
@@ -29,19 +33,16 @@ if (node_env === 'production') {
 			console.log('ENTORNO: ' + node_env);
 		});
 } else {
-	const puerto = envJSON[node_env].PORT_P;
-	const key = envJSON[node_env].KEY_SSL_P;
-	const cert = envJSON[node_env].CERT_SSL_P;
 	https
 		.createServer(
 			{
-				key: fs.readFileSync(key),
-				cert: fs.readFileSync(cert),
+				key: fs.readFileSync(keyDev),
+				cert: fs.readFileSync(certDev),
 			},
 			app
 		)
 		.listen(puerto, function() {
-			console.log('Servidor Ahurus Corriendo En: ' + puerto);
+			console.log('Servidor Ahurus Corriendo En: ' + puertoDev);
 			console.log('ENTORNO: ' + node_env);
 		});
 	/* const puerto = envJSON[node_env].PORT_D;
