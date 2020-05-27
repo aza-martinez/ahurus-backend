40df11a197678f7465362713c8d26da96031414f
SabinoRM <sabino.rendon.mireles@outlook.es>
2020-05-27 12:25:07 -0500

    TEST SAND

--- Master/helpers/PDFGenerator/config.js
+++ Master/helpers/PDFGenerator/config.js
@@ -11,7 +11,7 @@ module.exports = {
 	},
 	launcher: {
 		args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process', '--disable-extensions'],
-		headless: true,
+		headless: SVGComponentTransferFunctionElement,
 		//ignoreDefaultArgs: ['--disable-extensions'],
 	},
 	emulateMedia: 'screen',
