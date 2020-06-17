d5ed13395b4e97cc683ffabddd5a1f034917cb06
Azael Martinez <azael950620@gmail.com>
2020-06-17 12:03:18 -0500

    Mailing Design

--- Master/helpers/Mailer/index.js
+++ Master/helpers/Mailer/index.js
@@ -1,68 +1,80 @@
-const nodemailer = require('nodemailer');
-const PDFGenerator = require('../PDFGenerator/PDFGenerator');
-const path = require('path');
-const DateGenerator = require('../DateGenerator');
-const hbs = require('nodemailer-express-handlebars');
-const configMailer = require('./config');
-const { exec } = require('child_process');
+const nodemailer = require("nodemailer");
+const PDFGenerator = require("../PDFGenerator/PDFGenerator");
+const path = require("path");
+const DateGenerator = require("../DateGenerator");
+const hbs = require("nodemailer-express-handlebars");
+const configMailer = require("./config");
+const { exec } = require("child_process");
 
 class Mailer {
-	constructor(transferencia, centroCosto) {
-		this.centroCosto = centroCosto;
-		this.transferencia = transferencia;
+  constructor(transferencia, centroCosto) {
+    this.centroCosto = centroCosto;
+    this.transferencia = transferencia;
 
-		// functions
-		this.send = this.send.bind(this);
-		this.getTransporter = this.getTransporter.bind(this);
-		this.getMailOptions = this.getMailOptions.bind(this);
-	}
+    // functions
+    this.send = this.send.bind(this);
+    this.getTransporter = this.getTransporter.bind(this);
+    this.getMailOptions = this.getMailOptions.bind(this);
+  }
 
-	async send() {
-		// OBTENEMOS DATA GENERAL TRANSFERENCIA
-		const { fecha, hora } = DateGenerator.getDateAndHour(this.transferencia._doc.timestamp);
+  async send() {
+    // OBTENEMOS DATA GENERAL TRANSFERENCIA
+    const { fecha, hora } = DateGenerator.getDateAndHour(
+      this.transferencia._doc.timestamp
+    );
 
-		const cuentaBeneficiario = this.getBankAccount(this.transferencia._doc.cuentaBeneficiario);
-		console.log(cuentaBeneficiario);
-		const cuentaOrdenante = this.getBankAccount(this.transferencia._doc.cuentaOrdenante);
-		console.log(cuentaOrdenante);
-		const transferencia = {
-			...this.transferencia._doc,
-			...this.centroCosto,
-			fecha,
-			hora,
-			cuentaOrdenante,
-			cuentaBeneficiario,
-		};
+    const cuentaBeneficiario = this.getBankAccount(
+      this.transferencia._doc.cuentaBeneficiario
+    );
+    console.log(cuentaBeneficiario);
+    const cuentaOrdenante = this.getBankAccount(
+      this.transferencia._doc.cuentaOrdenante
+    );
+    console.log(cuentaOrdenante);
+    const transferencia = {
+      ...this.transferencia._doc,
+      ...this.centroCosto,
+      fecha,
+      hora,
+      cuentaOrdenante,
+      cuentaBeneficiario,
+    };
 
-		//SENDMAIL
-		const emailOrdenante = await this.sendMail(null, transferencia);
-		const emailBeneficiario = await this.sendMail(this.transferencia._doc.emailBeneficiario, transferencia, 'Beneficiario');
-		console.log(emailOrdenante);
-		console.log(emailBeneficiario);
-		return true;
-	}
+    //SENDMAIL
+    const emailOrdenante = await this.sendMail(null, transferencia);
 
-	async sendMail(email, contextEmail, type) {
-		// Creamos el transporter para el mail
-		const transporterOrdenante = await this.getTransporter();
-		//creamos configuración del template HTML
-		transporterOrdenante.use('compile', hbs(configMailer.engine));
-		const mailOptions = await this.getMailOptions(email, contextEmail, type);
-		// ENVIAMOS EMAIL
-		const transporte = await transporterOrdenante.sendMail(mailOptions);
-		console.log(transporte);
-		return transporte;
-	}
+    if (!this.transferencia._doc.descripcionError ||  this.transferencia._doc.descripcionError === '0') {
+      const emailBeneficiario = await this.sendMail(
+        this.transferencia._doc.emailBeneficiario,
+        transferencia,
+        "Beneficiario"
+      );
+    }
 
-	/**
+    return true;
+  }
+
+  async sendMail(email, contextEmail, type) {
+    // Creamos el transporter para el mail
+    const transporterOrdenante = await this.getTransporter();
+    //creamos configuración del template HTML
+    transporterOrdenante.use("compile", hbs(configMailer.engine));
+    const mailOptions = await this.getMailOptions(email, contextEmail, type);
+    // ENVIAMOS EMAIL
+    const transporte = await transporterOrdenante.sendMail(mailOptions);
+    console.log(transporte);
+    return transporte;
+  }
+
+  /**
    * getTansporter crea un Transporte de NodeMailer para poder
    * enviar el correo
    */
-	async getTransporter() {
-		return await nodemailer.createTransport(configMailer.transporter);
-	}
+  async getTransporter() {
+    return await nodemailer.createTransport(configMailer.transporter);
+  }
 
-	/**
+  /**
    * getBankAccount
    * @param {String} account
    *
@@ -70,13 +82,13 @@ class Mailer {
    * obteniendo los ultimos 4 digitos  y rellena el @param {account}
    * con "*"
    */
-	getBankAccount(account) {
-		const lastCharacters = account.substr(-4);
-		account = lastCharacters.padStart(account.length, '*');
-		return account;
-	}
+  getBankAccount(account) {
+    const lastCharacters = account.substr(-4);
+    account = lastCharacters.padStart(account.length, "*");
+    return account;
+  }
 
-	/**
+  /**
    * GetMailOptions nos retorna la configuración para el Mail
    *
    * @param {Object} context
@@ -84,36 +96,40 @@ class Mailer {
    * En caso de que el campo Exito = true
    * agrega la configuración para adjuntar archivos
    */
-	async getMailOptions(emailDestino, context, type) {
-		let pdfg;
+  async getMailOptions(emailDestino, context, type) {
+    let pdfg;
 
-		const templateMail = type === 'Beneficiario' ? configMailer.templateBeneficiario : configMailer.templateError;
-		const emailDestinatario = emailDestino || `${this.centroCosto.correo_contacto}`;
+    const templateMail =
+      type === "Beneficiario"
+        ? configMailer.templateBeneficiario
+        : configMailer.templateError;
+    const emailDestinatario =
+      emailDestino || `${this.centroCosto.correo_contacto}`;
 
-		const mailOptions = {
-			from: 'comprobantes@ahurus.com',
-			to: emailDestinatario,
-			subject: `Comprobante transferencia - ${this.transferencia.idSTP}`,
-			template: templateMail,
-			context,
-		};
+    const mailOptions = {
+      from: "Comprobantes Ahurus <comprobantes@ahurus.com>",
+      to: emailDestinatario,
+      subject: `Comprobante transferencia - ${this.transferencia.idSTP}`,
+      template: templateMail,
+      context,
+    };
 
-		if (!this.transferencia.descripcionError && type !== 'Beneficiario') {
-			mailOptions.template = configMailer.templateExito;
-			// obtenemos PDF
-			pdfg = new PDFGenerator(context);
-			const PDF = await pdfg.getPDF();
-			mailOptions.attachments = [
-				{
-					filename: `Recibo de pago - ${this.transferencia.idSTP}` + '.pdf',
-					content: PDF,
-					contentType: 'application/pdf',
-				},
-			];
-		}
+    if ((!this.transferencia.descripcionError || this.transferencia.descripcionError === '0') && type !== "Beneficiario") {
+      mailOptions.template = configMailer.templateExito;
+      // obtenemos PDF
+      pdfg = new PDFGenerator(context);
+      const PDF = await pdfg.getPDF();
+      mailOptions.attachments = [
+        {
+          filename: `Recibo de pago - ${this.transferencia.idSTP}` + ".pdf",
+          content: PDF,
+          contentType: "application/pdf",
+        },
+      ];
+    }
 
-		return mailOptions;
-	}
+    return mailOptions;
+  }
 }
 
 module.exports = Mailer;
